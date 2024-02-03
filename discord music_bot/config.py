5c098e45b7944fcfe88ffd7493c36deaea648282
富士 <78373273+yofujitsu@users.noreply.github.com>
2024-02-03 18:53:03 +0300

    container update

--- discord music_bot/config.py
+++ discord music_bot/config.py
@@ -1,1 +1,8 @@
-TOKEN="YOUR_TOKEN"
+import os
+
+# Получаем токен из переменной окружения
+TOKEN = os.getenv('TOKEN')
+
+# Проверяем, что токен был установлен
+if TOKEN is None:
+    raise ValueError("Не задан TOKEN. Укажите токен в переменной окружения TOKEN.")
